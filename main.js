b1f977b08a59cfb70346bbdfbc5d61b8060b7794
Gasstin <agustinruiz0202@gmail.com>
2023-02-18 02:56:34 -0300

    Primer commit, cambios en JS no funcionan

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
-var toggle = document.getElementById("container");
-var body = document.querySelector("body");
-var left = document.getElementsByName("left-area")
-
-
-toggle.onclick = function(){
-    toggle.classList.toggle("active");
-    body.classList.toggle("active");
-    left.classList.toggle("active")
-}
+let toggle=document.getElementById("toggle");
+toggle.addEventListener("cambio",(event)=>{
+    let cambiado = event.target.cambiado;
+    
+    if (cambiado==true){
+        body
+    }
+})
